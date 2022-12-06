9a1f0afe946e7ca7cb69666caf27d664967a8b10
Tomasz Wasielewski <79154397+tom-wasi@users.noreply.github.com>
2022-12-06 15:34:54 +0100

    Initial commit

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EscapeRoomManagerApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
